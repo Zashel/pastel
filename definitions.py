41c24e1c05f4c1918dc44450ea53a16bd81c9fce
Zashel <ivan.uria@gmail.com>
2017-04-04 13:26:08 +0200

    Definitions

---
+++ definitions.py
@@ -0,0 +1,31 @@
+PARI_FIELDS = ["id_cliente",
+               "id_cuenta",
+               "numdoc",
+               "fecha_factura",
+               "id_factura",
+               "importe_adeudado",
+               "estado_recibo"]
+
+PARI_UNIQUE = "id_factura"
+
+PAYMENTS_FIELDS = ["fecha",
+                   "importe",
+                   "observaciones",
+                   "dni",
+                   "id_cuenta",
+                   "tel1",
+                   "tel2",
+                   "oficina"]
+
+APLICATION_FIELDS = ["tipo", # Automático o Manual
+                     "pagos__id",
+                     "clientes_id_factura",
+                     "importe_aplicado",
+                     "nombre_cliente", # Manual
+                     "numdoc",
+                     "id_cuenta",
+                     "periodo_facturado",
+                     "metodos__id",
+                     "via_de_pago"] #Crear Objeto Relacional con esto
+
+METODOS_FIELDS = "nombre"

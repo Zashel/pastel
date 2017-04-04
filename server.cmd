9c43078bdeed6444abc4c032775ce680d04c3606
Zashel <ivan.uria@gmail.com>
2017-04-04 13:57:36 +0200

    Plotting application

---
+++ server.cmd
@@ -0,0 +1,1 @@
+Python\pastel.exe server.py
---
+++ server.py
@@ -0,0 +1,24 @@
+#! Python/pastel.exe
+
+from zrest.zrest.server import App, GET
+from zrest.zrest.datamodels.shelvemodels import ShelveModel, ShelveRelational
+from definitions import *
+
+import os
+
+PATH = "definitions"
+
+if __name__ == "__main__":
+    app = App()
+    app.set_method("shutdown", "^/shutdown$", GET, app.shutdown)
+    app.set_model(ShelveModel(os.path.join(PATH, "clientes"),
+                              index_fields=PARI_FIELDS,
+                              headers=PARI_FIELDS,
+                              unique=PARI_UNIQUE),
+                  "clientes",
+                  "^/clientes/<id_factura>$")
+    app.set_model(ShelveModel(os.path.join(PATH, "pagos"),
+                              index_fields=PAYMENTS_FIELDS,
+                              headers=PAYMENTS_FIELDS),
+                  "pagos",
+                  "^/pagos/<_id>$")

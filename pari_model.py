d9881bf3e849487cd5f45f66b558d9f8b8a28dc4
Zashel <ivan.uria@gmail.com>
2017-04-21 13:06:02 +0200

    debug

--- pari_model.py
+++ pari_model.py
@@ -337,4 +337,9 @@ class Pari(RestfulBaseInterface):
 
     @log
     def friend_fetch(self, data):
+        data["fecha_factura"] = str(int.from_bytes(data["fecha_factura"], "big"))
+        while len(data["fecha_factura"]) < 8:
+            data["fecha_factura"] = "0" + data["fecha_factura"]
+        data["segmento"] = self.indexes["segmento"][int.frombytes(data["segmento"], "big")]
+        data["estado_recibo"] = self.indexes["estado_recibo"][int.frombytes(data["estado_recibo"], "big")]
         return data

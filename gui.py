2009dd08d1ad70b3e0fa65c12ce607aecfb277a7
Zashel <ivan.uria@gmail.com>
2017-05-09 15:49:15 +0200

    Plotting GUI

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.ttk import *
+from collections import OrderedDict
 
 class TkVars:
     def __init__(self):
@@ -54,11 +56,14 @@ class App(Frame):
                             "total_posibles": IntVar(),
                             "estado": StringVar}
         self._pagos_list = [dict(self._active_pago) for x in range(50)]
-        self.widgets()
+        self._total_pagos_list = int()
         self.none_dict = {type(str()): "",
                           type(int()): 0,
                           type(float()): 0.0,
                           type(bool()): False}
+
+        self.widgets()
+        self.set_menu()
 
     def clean_pago(self, pago):
         for field in pago:
@@ -74,6 +79,7 @@ class App(Frame):
     def clean_pagos_list(self):
         for item in self._pagos_list:
             self.clean_pago(item)
+        self._total_pagos_list = int()
 
     def set_pago(self, pago, data):
         pago["link"].set(data["_links"]["self"]["href"])
@@ -110,16 +116,95 @@ class App(Frame):
                 elif head == "importe_str":
                     dato = str(pago["posible"][s_index][head]["importe"].get())
                     dato = "{},{} €".format(dato[:-2], dato[-2:])
-                    pago["posible"][s_index][head].set(datos[dato])
+                pago["posible"][s_index][head].set(dato)
 
     def set_pagos_list(self, data):
         self.clean_pagos_list()
         for index, item in enumerate(data):
             self._pagos_list[index]["index"].set(index)
             self.set_pago(self.pagos_list[index], item)
+        self._total_pagos_list = len(data)
 
     def widgets(self):
-        pass
+        #TABS
+        self.tabs = {"init": Frame(self),
+                     "configuration": Frame(self),
+                     "payments": Frame(self)}
+
+        #Payments Tree
+        columns = OrderedDict({"estado": [100, "Estado"],
+                               "fecha": [100, "Fecha Pago"],
+                               "importe_str": [100, "Importe"],
+                               "dni": [100, "DNI"],
+                               "id_cliente": [100, "ID Cliente"],
+                               "tels": [100, "Teléfonos"],
+                               "oficina": [100, "Oficina"],
+                               "observaciones": [100, "Observaciones"]})
+        self.payments_tree_frame = Frame(self.tabs["payments"])
+        self.payments_tree_frame.pack()
+        self.payments_tree = Treeview(self.payments_tree_frame, columns=list(columns.keys()))
+        self.payments_tree.column("#0", width=100)
+        self.payments_tree.heading("#0", text="ID")
+        for column in columns:
+            self.payments_tree.column(column, width=columns[column][0])
+            self.payments_tree.heading(column, text=columns[column][1])
+        self.payments_tree.column("estado", width=100)
+        self.payments_tree.pack()
+
+        self.payment_frame = Frame(self.tabs["payments"])
+        self.payment_frame.tkraise(self.payments_tree_frame)
+        Button(self.payment_frame, text="Cerrar", command=self.hide_payment).pack()
+
+    def hide_payment(self):
+        self.payment_frame.pack_forget()
+        self.payments_tree.pack()
+
+    def hide_payment_tree(self):
+        self.payments_tree.pack_forget()
+        self.payment_frame.pack()
+
+    def update_payments_tree(self):
+        for index in range(self._total_pagos_list):
+            pass
+
+    def set_menu(self):
+        self.option_add("*tearOff", FALSE)
+        win = self.winfo_toplevel()
+        self.menubar = Menu(win)
+        win["menu"] = self.menubar
+
+        self.menu_file = Menu(self.menubar)
+        self.menu_edit = Menu(self.menubar)
+        self.menu_open = Menu(self.menubar)
+        self.menu_new = Menu(self.menubar)
+        self.menu_load = Menu(self.menubar)
+
+        self.menubar.add_cascade(menu=self.menu_file, label="Archivo")
+        self.menubar.add_cascade(menu=self.menu_edit, label="Edición")
+
+        self.menu_file.add_cascade(menu=self.menu_new, label="Nuevo")
+        self.menu_file.add_cascade(menu=self.menu_open, label="Abrir")
+        self.menu_file.add_cascade(menu=self.menu_load, label="Cargar")
+        self.menu_file.add_command(label="Exportar...")
+        self.menu_file.add_separator()
+        self.menu_file.add_command(label="Cambiar Usuario")
+
+        self.menu_new.add_command(label="Compromiso")
+        self.menu_new.add_command(label="Localización")
+        self.menu_new.add_command(label="Pago Pasarela")
+        self.menu_new.add_command(label="Usuario")
+
+        self.menu_open.add_command(label="Compromisos")
+        self.menu_open.add_command(label="Pagos")
+        self.menu_open.add_command(label="Usuarios")
+
+        self.menu_load.add_command(label="Pagos ISM")
+        self.menu_load.add_command(label="PARI...")
+        self.menu_load.add_command(label="Último PARI")
+
+        self.menu_edit
+
+
 
     @property
     def vars(self):
@@ -129,5 +214,5 @@ class App(Frame):
 if __name__ == "__main__":
     root = Tk()
     app = App(root)
-    print(app.vars.string.get())
+    print(Frame.__module__)
     app.mainloop()

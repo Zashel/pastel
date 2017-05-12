03654d9602d9e3125e936d9b2c4400fdd537a338
Zashel <ivan.uria@gmail.com>
2017-05-12 14:18:17 +0200

    debug

--- tkutils.py
+++ tkutils.py
@@ -178,7 +178,7 @@ class EasyFrame(Frame):
                 column_config.update(default_config["column"][column])
             tree.column(column, **column_config)
             if "heading" in default_config:
-                tree.heading(column, **default_config["heading"])
+                tree.heading(column, **default_config["heading"][column])
         if "bind" in default_config:
             for item in default_config["bind"]:
                 tree.bind(item, default_config["bind"][item])

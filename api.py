f1729d51b8874603f03fa0094917e761251ef6b7
Zashel <ivan.uria@gmail.com>
2017-05-16 15:15:08 +0200

    debug implementation of LinkButton and TreeView

--- api.py
+++ api.py
@@ -95,7 +95,7 @@ class API:
                                                               ))
         if request.status_code == 200:
             data = json.loads(request.text)
-            API.pagos["cache"] = data["_embedded"]["pagos"]
+            API.pagos["cache"] = data["pagos"]
             if "_links" in data:
                 for link in ("self", "next", "prev", "first", "last"):
                     if link in data["_links"]:
